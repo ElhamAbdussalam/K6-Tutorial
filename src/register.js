8f64923c90c9ff6b7aaf15991ee72788f01645f9
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-26 19:53:42 +0700

    http request

--- src/register.js
+++ src/register.js
@@ -20,4 +20,31 @@ export default function () {
       "Content-Type": "application/json",
     },
   });
+
+  const loginBody = {
+    username: `user-${uniqueId}`,
+    password: "rahasia",
+  };
+
+  const response = http.post(
+    "http://localhost:3000/api/users/login",
+    JSON.stringify(loginBody),
+    {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    },
+  );
+
+  const responseBody = response.json();
+
+  const currentResponse = http.get("http://localhost:3000/api/users/current", {
+    headers: {
+      Accept: "application/json",
+      Authorization: responseBody.data.token,
+    },
+  });
+
+  const currentBody = currentResponse.json();
 }
